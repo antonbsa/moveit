f033955f1a623dc3ba89f93a7e706a9a08adc1bf
Anton <antonbsa@gmail.com>
2021-03-07 02:39:25 -0300

    setting resetChallenge, using dynamic experience data

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -11,9 +11,11 @@ interface ChallengesContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
+    experienceToNextLevel: number;
     activeChallenge: Challenge;
     levelUp: () => void;
     startNewChallenge: () => void;
+    resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -26,7 +28,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
+
     const [activeChallenge, setActiveChallenge] = useState(null);
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
     function levelUp() {
         setLevel(level + 1);
@@ -39,15 +44,21 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         setActiveChallenge(challenge);
     }
 
+    function resetChallenge() {
+        setActiveChallenge(null);
+    }
+
     return (
         <ChallengesContext.Provider
             value={{
                 level,
                 currentExperience,
                 challengesCompleted,
+                experienceToNextLevel,
+                activeChallenge,
                 levelUp,
                 startNewChallenge,
-                activeChallenge
+                resetChallenge
             }}>
             {children}
         </ChallengesContext.Provider>
