591559e33e2d00022362946ac21beeb631e95c33
Anton <antonbsa@gmail.com>
2021-03-07 13:24:54 -0300

    fix text challengeBox and css gray-line

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -36,7 +36,7 @@ export function ChallengeBox() {
                 </div>
             ) : (
                 <div className={styles.challengeNotActive}>
-                    <strong>Finalize um cicl para receber um desafio</strong>
+                    <strong>Finalize um ciclo para receber um desafio</strong>
                     <p>
                         <img src="icons/level-up.svg" alt="Level up"/>
                         Avance de level completando desafios.
